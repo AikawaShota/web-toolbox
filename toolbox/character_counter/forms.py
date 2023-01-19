a75c08442b31d327f7593de63f1b6e2c7c4f8044
AikawaShota <aikawa.shota24@gmail.com>
2023-01-19 15:10:38 +0900

    add css and html

--- toolbox/character_counter/forms.py
+++ toolbox/character_counter/forms.py
@@ -2,4 +2,10 @@ from django import forms
 
 
 class CounterForm(forms.Form):
-    text = forms.CharField(widget=forms.Textarea(attrs={'class': 'text_field'}))
+    text = forms.CharField(widget=forms.Textarea(
+        attrs={
+            'class': 'text_field',
+            'placeholder': '文字数をカウントしたい文章をここに入力。',
+            'cols': '64',
+            'lows': '10',
+        }))
