21e0ce44cf610060f978969a7b1a4f5b48f790e7
IncogNEET <incogneet@vivaldi.net>
2023-01-26 16:09:00 +0900

    アクティブなページをサイドバー上でハイライトするようにした

--- toolbox/character_counter/forms.py
+++ toolbox/character_counter/forms.py
@@ -5,7 +5,7 @@ class CounterForm(forms.Form):
     text = forms.CharField(widget=forms.Textarea(
         attrs={
             'class': 'text_field',
-            'placeholder': '文字数をカウントしたい文章をここに入力。',
+            'placeholder': 'カウントしたい文章を入力してください',
             'cols': '64',
             'lows': '10',
         }))
